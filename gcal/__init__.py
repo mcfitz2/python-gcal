8ac413d28139605cda7fcd9382111639b75a72dc
Micah Fitzgerald <mcfitz2@gmail.com>
2013-10-16 16:42:00 -0400

    bugfixes

--- gcal/__init__.py
+++ gcal/__init__.py
@@ -2,7 +2,7 @@ import gflags
 import httplib2
 from apiclient.discovery import build
 from oauth2client.file import Storage
-from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.client import OAuth2WebServerFlow, AccessTokenCredentials
 from oauth2client.tools import run
 import dateutil.parser
 import pytz
@@ -19,8 +19,9 @@ class GCal(object):
         http = httplib2.Http()
         http = credentials.authorize(http)
         self.service = build(serviceName='calendar', version='v3', http=http)
-        self.calendars = self.service.calendarList().list().execute()    
-        self.event_cache = {}
+        self.calendars = None 
+        self.api_calls = 0
+        self.cache = []
     def __parse_dates(self, event):
         for key in ["start", "end"]:
             if event[key].get("date", False):
@@ -31,9 +32,12 @@ class GCal(object):
                 event[key]["dateTime"] = dateutil.parser.parse(event[key]["dateTime"]).astimezone(pytz.UTC)
                 continue
         return event
-    def get_events(self, timeMin, timeMax):
+    def get_events(self, timeMin, timeMax, calendars=None):
         ret = []
-        for c in self.calendars["items"]:
+        if not self.calendars:
+            self.calendars = self.service.calendarList().list().execute()    
+        cals = [{"id":cal} for cal in calendars] if calendars else self.calendars["items"]
+        for c in  cals:
             events = self.service.events().list(
                 calendarId=c["id"], 
                 singleEvents=True, 
@@ -43,10 +47,14 @@ class GCal(object):
                 timeMax=timeMax.isoformat(),
                 ).execute()
             ret.extend(filter(lambda event: not event.get("allDay", False), map(self.__parse_dates, events["items"])))
- 
+            self.api_calls += 1
         return ret
     def busy(self, start, stop, calendars=None):
+        
         if not calendars:
+            if not self.calendars:
+                self.calendars = self.service.calendarList().list().execute()    
+                self.api_calls += 1
             calendars = [{"id":cal["id"]} for cal in self.calendars["items"]]
         fb = self.service.freebusy().query(body={"timeMin":start.isoformat(), 
                                                "timeMax":stop.isoformat(), 
@@ -54,10 +62,11 @@ class GCal(object):
                                                "groupExpansionMax":10,
                                                "calendarExpansionMax":10}
                                          ).execute()
-        print fb
+        self.api_calls += 1
         return any(map(lambda x: len(x["busy"]) > 0, fb["calendars"].itervalues()))
 
     def exists(self, event, calendars=None):
+        
         start = event["start"].get("date", False) or event["start"].get("dateTime")
         end = event["end"].get("date", False) or event["end"].get("dateTime")
         events = self.get_events(start, end)
@@ -68,14 +77,11 @@ class GCal(object):
                 e_start = dateutil.parser.parse(e_start)
             if isinstance(e_end, str):
                 e_end = dateutil.parser.parse(e_end)
-
-#            print e["summary"], "==", event["summary"]
-#            print e_start, "==", start
-#            print e_end, "==", end
-#            print "-"*40    
             if e["summary"] == event["summary"] and e_start == start and e_end == end:
                 return True
         return False
     def add_event(self, calendarId, name, start, end, location=""):
+        self.api_calls += 1
         return self.service.events().insert(calendarId=calendarId, body={"start":{"dateTime":start.isoformat()}, "end":{"dateTime":end.isoformat()}, "location":location, "summary":name}).execute()
+    
     
