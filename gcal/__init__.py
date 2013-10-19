b29a894580732aba7dfff7ba58587da990a9281d
Micah Fitzgerald <mcfitz2@gmail.com>
2013-10-19 18:45:02 -0400

    bugfixes

--- gcal/__init__.py
+++ gcal/__init__.py
@@ -7,6 +7,10 @@ from oauth2client.tools import run
 import dateutil.parser
 import pytz
 import datetime
+import time
+import logging
+logger = logging.getLogger('')
+logger.setLevel(logging.ERROR)
 class GCal(object):
     def __init__(self, client_id=None, client_secret=None, scope=None, user_agent=None):
         FLAGS = gflags.FLAGS
@@ -38,15 +42,21 @@ class GCal(object):
             self.calendars = self.service.calendarList().list().execute()    
         cals = [{"id":cal} for cal in calendars] if calendars else self.calendars["items"]
         for c in  cals:
-            events = self.service.events().list(
+            def add_cal_id(event):
+                event["calendarId"] = c["id"]
+                return event
+
+            req = self.service.events().list(
                 calendarId=c["id"], 
                 singleEvents=True, 
                 maxResults=1000, 
                 orderBy='startTime',
-                timeMin=timeMin.isoformat(), 
-                timeMax=timeMax.isoformat(),
-                ).execute()
-            ret.extend(filter(lambda event: not event.get("allDay", False), map(self.__parse_dates, events["items"])))
+                timeMin=timeMin.isoformat("T")+"Z", 
+                timeMax=timeMax.isoformat("T")+"Z",
+                )
+     #       print dir(req)
+            events = req.execute()
+            ret.extend(map(add_cal_id, filter(lambda event: not event.get("allDay", False), map(self.__parse_dates, events["items"]))))
             self.api_calls += 1
         return ret
     def busy(self, start, stop, calendars=None):
@@ -56,6 +66,8 @@ class GCal(object):
                 self.calendars = self.service.calendarList().list().execute()    
                 self.api_calls += 1
             calendars = [{"id":cal["id"]} for cal in self.calendars["items"]]
+        else:
+            calendars = [{"id":cal} for cal in calendars]
         fb = self.service.freebusy().query(body={"timeMin":start.isoformat(), 
                                                "timeMax":stop.isoformat(), 
                                                "items":calendars,
@@ -65,11 +77,11 @@ class GCal(object):
         self.api_calls += 1
         return any(map(lambda x: len(x["busy"]) > 0, fb["calendars"].itervalues()))
 
-    def exists(self, event, calendars=None):
-        
+    def exists(self, event, calendars=None):        
         start = event["start"].get("date", False) or event["start"].get("dateTime")
         end = event["end"].get("date", False) or event["end"].get("dateTime")
-        events = self.get_events(start, end)
+        events = self.get_events(start, end, calendars=calendars)
+        ret = []
         for e in events:
             e_start = e["start"].get("date", False) or e["start"].get("dateTime")
             e_end = e["end"].get("date", False) or e["end"].get("dateTime")
@@ -78,10 +90,31 @@ class GCal(object):
             if isinstance(e_end, str):
                 e_end = dateutil.parser.parse(e_end)
             if e["summary"] == event["summary"] and e_start == start and e_end == end:
-                return True
-        return False
+                ret.append(e)
+        return (True, ret) if len(ret) > 0 else (False, None)
     def add_event(self, calendarId, name, start, end, location=""):
         self.api_calls += 1
         return self.service.events().insert(calendarId=calendarId, body={"start":{"dateTime":start.isoformat()}, "end":{"dateTime":end.isoformat()}, "location":location, "summary":name}).execute()
+    def delete_event(self, calendarId, eventId):
+        self.api_calls += 1
+        return self.service.events().delete(calendarId=calendarId, eventId=eventId).execute()
     
-    
+class GoogleEvent(dict):
+    def __init__(self, event_dict):
+        dict.__init__(self, event_dict)
+        start = event_dict["start"].get("date", False) or event_dict["start"].get("dateTime")
+        end = event_dict["end"].get("date", False) or event_dict["end"].get("dateTime")
+        if isinstance(start, str):
+            start = dateutil.parser.parse(start)
+        if isinstance(end, str):
+            end = dateutil.parser.parse(end)
+        name = event_dict["summary"]
+        self.name = name
+        self.start = start
+        self.end = end
+    def __repr__(self):
+        event = self.toEastern()
+        return "%s - %s from %s to %s" % (event.name, event.start.strftime("%a, %b %d %Y"), event.start.strftime("%I:%M%p"), event.end.strftime("%I:%M%p"))
+    def toEastern(self):
+        return StandardEvent(self.name, self.start.astimezone(pytz.timezone("US/Eastern")), self.end.astimezone(pytz.timezone("US/Eastern")))
+
